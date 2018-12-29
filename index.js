a719a70ccff5d2ed3295160a53bd9f16380b6fd8
Vladimir Varavva <vladimir.varavva@masterofcode.com>
2018-12-29 15:39:32 +0200

    ref

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const AWS = require('aws-sdk');
 
 class S3RemoveFilesPlugin {
     constructor(options) {
+        AWS.config.update(options.config);
         this.name = 'S3RemoveFilesPlugin';
         this.options = options;
+        this.s3 = new AWS.S3();
         this.params = {
             Bucket: this.options.params.Bucket,
             Delete: { Objects: [] }
@@ -14,41 +16,37 @@ class S3RemoveFilesPlugin {
         compiler.hooks.afterEmit.tapPromise(this.name, this.handle.bind(this));
     }
 
-    handle() {
-        this.config();
-        this.connect();
-
-        return this.removeFiles();
+    handle(compilation) {
+        return this.removeData(compilation);
     }
 
-    config() {
-        AWS.config.update(this.options.config);
-    }
+    async removeData(compilation) {
+        this.data = await this.getData();
+        if (!this.data.Contents.length) return;
 
-    connect() {
-        this.s3 = new AWS.S3();
-    }
 
-    async removeFiles() {
-        let listedObjects = await this.s3.listObjectsV2(this.options.params).promise();
-
-        if (!listedObjects.Contents.length) return;
-
-        listedObjects.Contents.forEach(({ Key }) => {
-            if (this.reg.test(Key)) return;
-            this.params.Delete.Objects.push({ Key });
-        });
+        this.prepareData(compilation);
 
         await this.s3.deleteObjects(this.params).promise();
-
-        if (listedObjects.IsTruncated) await this.removeFiles();
+        if (this.data.IsTruncated) await this.removeData();
     }
 
-    get reg() {
+    getData() {
+        return this.s3.listObjectsV2(this.options.params).promise();
+    }
+
+    prepareData(compilation) {
+        this.data.Contents.forEach(({ Key }) => {
+            if (this.reg(compilation).test(Key)) return;
+            this.params.Delete.Objects.push({ Key });
+        });
+    }
+
+    reg(compilation) {
         let str = '';
 
-        for (let i = 0; i < this.options.count; i++) {
-            str += process.env[this.options.var] - i + '|';
+        for (let i = 0; i < this.options.count + 1; i++) {
+            str += compilation[this.options.var] - i + '|';
         }
 
         return new RegExp(`\\.(${str})\\.`);
